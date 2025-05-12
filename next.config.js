efd2bd2b27bb6dd7dee0539db5c810ea26e10985
PasserByJia <873750974@qq.com>
2025-05-13 00:11:46 +0800

    fix: fix pdf2md

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 // 最佳实践配置示例
 module.exports = {
   experimental: {
-    serverComponentsExternalPackages: ['@opendocsg/pdf2md', 'pdfjs-dist']
+    serverComponentsExternalPackages: ['@opendocsg/pdf2md', '@napi-rs/canvas', 'pdfjs-dist', '@tiny-tool/pdf2md'],
+    esmExternals: 'loose'
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -10,7 +11,9 @@ module.exports = {
         'pdfjs-dist': 'window.pdfjsLib'
       });
     } else {
-      config.externals.push('canvas');
+      config.externals.push('@napi-rs/canvas');
+      config.externals.push('pdfjs-dist');
+      config.externals.push('@tiny-tool/pdf2md');
     }
     return config;
   }
