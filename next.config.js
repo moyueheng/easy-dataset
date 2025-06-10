7efb00868617b89be9700501ed06b796fb975889
PasserByJia <873750974@qq.com>
2025-06-10 18:33:36 +0800

    fix: dependencie

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // 最佳实践配置示例
 module.exports = {
   experimental: {
-    serverComponentsExternalPackages: ['@opendocsg/pdf2md', '@napi-rs/canvas', 'pdfjs-dist'],
+    serverComponentsExternalPackages: ['@opendocsg/pdf2md', 'pdfjs-dist', '@hyzyla/pdfium'],
     esmExternals: 'loose'
   },
   webpack: (config, { isServer }) => {
@@ -11,8 +11,8 @@ module.exports = {
         'pdfjs-dist': 'window.pdfjsLib'
       });
     } else {
-      config.externals.push('@napi-rs/canvas');
       config.externals.push('pdfjs-dist');
+      config.externals.push('@hyzyla/pdfium');
     }
     return config;
   }
